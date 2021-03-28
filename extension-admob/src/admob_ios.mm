dde8a6fdc86e53e65681535058006483dc3a8564
Alexey Gulev <me@agulev.com>
2021-03-28 21:44:56 +0200

    ios base skeleton

---
+++ extension-admob/src/admob_ios.mm
@@ -0,0 +1,63 @@
+#if defined(DM_PLATFORM_IOS)
+#include "admob_private.h"
+#include "admob_callback_private.h"
+
+#include <GoogleMobileAds/GoogleMobileAds.h>
+
+namespace dmAdmob {
+
+    void Initialize_Ext() {
+
+    }
+
+    void Initialize() {
+        
+    }
+
+    void LoadInterstitial(const char* unitId){
+        
+    }
+
+    void ShowInterstitial() {
+        
+    }
+
+    void LoadRewarded(const char* unitId) {
+        
+    }
+
+    void ShowRewarded() {
+        
+    }
+
+    void LoadBanner(const char* unitId, BannerSize bannerSize) {
+
+    }
+
+    void DestroyBanner() {
+        
+    }
+
+    void ShowBanner(BannerPosition bannerPos) {
+        
+    }
+
+    void HideBanner() {
+        
+    }
+
+    bool IsInterstitialLoaded() {
+
+    }
+
+    bool IsRewardedLoaded() {
+        
+    }
+
+    bool IsBannerLoaded() {
+        
+    }
+
+} //namespace
+
+#endif
