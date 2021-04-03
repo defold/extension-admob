80819bc2225cfc06f88480070121750452b99d20
Alexey Gulev <me@agulev.com>
2021-04-03 15:31:23 +0200

    ios interstitial ads

--- extension-admob/src/admob_ios.mm
+++ extension-admob/src/admob_ios.mm
@@ -3,9 +3,14 @@
 #include "admob_callback_private.h"
 
 #include <GoogleMobileAds/GoogleMobileAds.h>
-#include <Foundation/Foundation.h>
+#include <UIKit/UIKit.h>
+
+@interface AdmobExtInterstitialAdDelegate : NSObject<GADFullScreenContentDelegate>
+@end
 
 namespace dmAdmob {
+
+    static UIViewController *uiViewController = nil;
 
     void SendSimpleMessageObjCObject(MessageId msg, id obj) {
         NSError* error;
@@ -36,30 +41,96 @@ namespace dmAdmob {
         }
     }
 
-    void SendSimpleMessageInt(MessageId msg, MessageEvent event) {
+    void SendSimpleMessage(MessageId msg, MessageEvent event) {
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
         [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
         SendSimpleMessageObjCObject(msg, dict);
     }
 
-    void Initialize_Ext() {
+    void SendSimpleMessageIntString(MessageId msg, MessageEvent event, NSString *key_2, int value_2, NSString *key_3, NSString *value_3) {
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
+        [dict setObject:[NSNumber numberWithInt:value_2] forKey:key_2];
+        [dict setObject:value_3 forKey:key_3];
+        SendSimpleMessageObjCObject(msg, dict);
+    }
 
+    void SendSimpleMessageString(MessageId msg, MessageEvent event, NSString *key_2, NSString *value_2) {
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
+        [dict setObject:value_2 forKey:key_2];
+        SendSimpleMessageObjCObject(msg, dict);
     }
 
     void Initialize() {
         [[GADMobileAds sharedInstance]
-            startWithCompletionHandler:^(GADInitializationStatus *_Nonnull status) {
-              SendSimpleMessageInt(MSG_INITIALIZATION, EVENT_COMPLETE);
+        startWithCompletionHandler:^(GADInitializationStatus *_Nonnull status) {
+            SendSimpleMessage(MSG_INITIALIZATION, EVENT_COMPLETE);
+        }];
+    }
+
+//--------------------------------------------------
+// Interstitial ADS
+
+    static GADInterstitialAd *interstitialAd = nil;
+    static AdmobExtInterstitialAdDelegate *admobExtInterstitialAdDelegate;
+
+    void SetInterstitialAd(GADInterstitialAd *newAd) {
+        if (interstitialAd == newAd) {
+            return;
+        }
+        if (newAd != nil) {
+            [newAd retain];
+        }
+        if (interstitialAd != nil) {
+            [interstitialAd release];
+        }
+        interstitialAd = newAd;
+    }
+
+    void LoadInterstitial(const char* unitId) {
+        GADRequest *request = [GADRequest request];
+        [GADInterstitialAd
+            loadWithAdUnitID:[NSString stringWithUTF8String:unitId]
+            request:request
+            completionHandler:^(GADInterstitialAd *ad, NSError *error) {
+                if (error) {
+                    SetInterstitialAd(nil);
+                    NSLog([NSString stringWithFormat:@"Error domain: \"%@\". %@", [error domain], [error localizedDescription]]);
+                    SendSimpleMessageIntString(MSG_INTERSTITIAL, EVENT_FAILED_TO_LOAD, @"code", [error code],
+                          @"error", [NSString stringWithFormat:@"Error domain: \"%@\". %@", [error domain], [error localizedDescription]]);
+                    return;
+                }
+                SetInterstitialAd(ad);
+                SendSimpleMessage(MSG_INTERSTITIAL, EVENT_LOADED);
             }];
     }
 
-    void LoadInterstitial(const char* unitId){
-        
+    bool IsInterstitialLoaded() {
+        return interstitialAd != nil;
     }
 
     void ShowInterstitial() {
-        
+        if (IsInterstitialLoaded()) {
+            interstitialAd.fullScreenContentDelegate = admobExtInterstitialAdDelegate;
+            NSError* error;
+            if ([interstitialAd canPresentFromRootViewController:uiViewController error:&error]) {
+                [interstitialAd presentFromRootViewController:uiViewController];
+            } else {
+                if (error) {
+                    SendSimpleMessageIntString(MSG_INTERSTITIAL, EVENT_FAILED_TO_LOAD, @"code", [error code],
+                          @"error", [NSString stringWithFormat:@"Error domain: \"%@\". %@", [error domain], [error localizedDescription]]);
+                } else {
+                    SendSimpleMessageString(MSG_INTERSTITIAL, EVENT_NOT_LOADED, @"error", @"Can't present Interstitial AD");
+                }
+            }
+        } else {
+            SendSimpleMessageString(MSG_INTERSTITIAL, EVENT_NOT_LOADED, @"error", @"Can't show Interstitial AD that wasn't loaded.");
+        }
     }
+
+//--------------------------------------------------
+// Rewarded ADS
 
     void LoadRewarded(const char* unitId) {
         
@@ -68,6 +139,13 @@ namespace dmAdmob {
     void ShowRewarded() {
         
     }
+
+    bool IsRewardedLoaded() {
+        return false;
+    }
+
+//--------------------------------------------------
+// Banner ADS
 
     void LoadBanner(const char* unitId, BannerSize bannerSize) {
 
@@ -85,18 +163,38 @@ namespace dmAdmob {
         
     }
 
-    bool IsInterstitialLoaded() {
-
-    }
-
-    bool IsRewardedLoaded() {
-        
-    }
-
     bool IsBannerLoaded() {
-        
+        return false;
     }
+
+//--------------------------------------------------
+
+void Initialize_Ext() {
+    UIWindow* window = dmGraphics::GetNativeiOSUIWindow();
+    uiViewController = window.rootViewController;
+
+    admobExtInterstitialAdDelegate = [[AdmobExtInterstitialAdDelegate alloc] init];
+}
 
 } //namespace
+
+@implementation AdmobExtInterstitialAdDelegate
+
+- (void)adDidDismissFullScreenContent:(id)ad {
+    dmAdmob::SetInterstitialAd(nil);
+    dmAdmob::SendSimpleMessage(dmAdmob::MSG_INTERSTITIAL, dmAdmob::EVENT_CLOSED);
+}
+
+- (void)ad:(id)ad didFailToPresentFullScreenContentWithError:(NSError *)error {
+    dmAdmob::SetInterstitialAd(nil);
+    dmAdmob::SendSimpleMessageIntString(dmAdmob::MSG_INTERSTITIAL, dmAdmob::EVENT_FAILED_TO_SHOW, @"code", [error code],
+        @"error", [NSString stringWithFormat:@"Error domain: \"%s\". %s", [error domain], [error localizedDescription]]);
+}
+
+- (void)adDidPresentFullScreenContent:(id)ad {
+    dmAdmob::SendSimpleMessage(dmAdmob::MSG_INTERSTITIAL, dmAdmob::EVENT_OPENING);
+}
+
+@end
 
 #endif
