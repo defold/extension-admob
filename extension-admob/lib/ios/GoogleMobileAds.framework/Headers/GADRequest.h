29dbc224ff4a7b94b04032017a3d185fdbfd114c
Alexey Gulev <me@agulev.com>
2021-12-03 23:04:15 +0100

    Android SDK 20.5.0 and iOS SDK 8.13.0

--- extension-admob/lib/ios/GoogleMobileAds.framework/Headers/GADRequest.h
+++ extension-admob/lib/ios/GoogleMobileAds.framework/Headers/GADRequest.h
@@ -12,7 +12,8 @@
 #import <UIKit/UIKit.h>
 
 /// Add this constant to the testDevices property's array to receive test ads on the simulator.
-GAD_EXTERN const id _Nonnull kGADSimulatorID;
+GAD_EXTERN NSString *_Nonnull const kGADSimulatorID GAD_DEPRECATED_MSG_ATTRIBUTE(
+    "Use GADRequestConfiguration/GADSimulatorID instead.");
 
 /// Deprecated gender constants.
 typedef NS_ENUM(NSInteger, GADGender) {
@@ -62,7 +63,7 @@ typedef NS_ENUM(NSInteger, GADGender) {
 
 /// Array of keyword strings. Keywords are words or phrases describing the current user activity
 /// such as @"Sports Scores" or @"Football". Set this property to nil to clear the keywords.
-@property(nonatomic, copy, nullable) NSArray *keywords;
+@property(nonatomic, copy, nullable) NSArray<NSString *> *keywords;
 
 /// URL string for a webpage whose content matches the app's primary content. This webpage content
 /// is used for targeting and brand safety purposes.
